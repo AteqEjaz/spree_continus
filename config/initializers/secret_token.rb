0a2e08c50b0dea1ee9bc674a6265bfffbe4b8dfc
AteqEjaz <ateq.odesk@gmail.com>
2013-08-18 09:29:13 +0900

    Spree_continus initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpreeBasic::Application.config.secret_token = 'ade6c70ea435a77b61ac97a4b12537bd6a68d03d744eef8b4b96492130bffedbac84816692474041ab100a10ed79307de039a323886c9760e4f37f973040d297'
